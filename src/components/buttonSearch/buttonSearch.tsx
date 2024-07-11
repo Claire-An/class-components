94f89152dce99074f485cadc5d61dafde6aac178
Svetlana <Claire-An@mail.ru>
2024-07-12 03:39:19 +0500

    feat: conversion of class componens to functional components

--- src/components/buttonSearch/buttonSearch.tsx
+++ src/components/buttonSearch/buttonSearch.tsx
@@ -1,43 +1,30 @@
-import { Component } from 'react';
 import { getData } from '../../api';
 import { Character } from '../../api/types';
 import styles from './buttonSearch.module.scss';
-
-interface State {
-  value: string;
-}
 
 interface Props {
   value: string;
   onButtonClick: (val: { data: Character[] }) => void;
 }
 
-class ButtonSearch extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      value: props.value,
-    };
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
+const ButtonSearch = (props: Props) => {
+  const { value, onButtonClick } = props;
 
-  async handleButtonClick() {
-    localStorage.setItem('textSearch', this.props.value);
-    const data = await getData(this.props.value);
+  const handleButtonClick = async () => {
+    localStorage.setItem('textSearch', value);
+    const data = await getData(value);
     if (data) {
-      this.props.onButtonClick(data);
+      onButtonClick(data);
     } else {
-      this.props.onButtonClick({ data: [] });
+      onButtonClick({ data: [] });
     }
-  }
+  };
 
-  render() {
-    return (
-      <button className={styles.buttonSearch} onClick={this.handleButtonClick}>
-        Найти
-      </button>
-    );
-  }
-}
+  return (
+    <button className={styles.buttonSearch} onClick={handleButtonClick}>
+      Найти
+    </button>
+  );
+};
 
 export default ButtonSearch;
--- src/page/homePage.tsx
+++ src/page/homePage.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { getData } from '../api';
 import { Character } from '../api/types';
 import ButtonSearch from '../components/buttonSearch/buttonSearch';
@@ -7,78 +7,57 @@ import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 import InputSearch from '../components/inputSearch/inputSearch';
 import styles from './homePage.module.scss';
 
-export interface HomeProps {
-  value?: string;
-}
+const HomePage: React.FC = () => {
+  const [value, setValue] = useState(localStorage.getItem('textSearch') || '');
+  const [data, setData] = useState<Character[]>([]);
+  const [hasError, setHasError] = useState(false);
 
-export interface HomeState {
-  data: Character[];
-  value: string;
-  hasError: boolean;
-}
-
-class HomePage extends Component<HomeProps, HomeState> {
-  constructor(props: HomeProps) {
-    super(props);
-    this.state = {
-      data: [],
-      value: localStorage.getItem('textSearch') || '',
-      hasError: false,
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleButtonErrorClick = this.handleButtonErrorClick.bind(this);
-  }
-
-  handleInputChange(val: string) {
-    this.setState({ value: val });
-  }
-
-  async componentDidMount(): Promise<void> {
-    const data = await getData(this.state.value);
-    if (data) {
-      this.setState(data);
-    } else {
-      this.setState({ data: [] });
-    }
-  }
-
-  handleButtonClick = async (val: { data: Character[] }): Promise<void> => {
-    this.setState({ data: val.data });
+  const handleInputChange = (val: string) => {
+    setValue(val);
   };
 
-  handleButtonErrorClick(): void {
-    this.setState({ hasError: true });
-  }
+  useEffect(() => {
+    const getCharacter = async () => {
+      const dataCharacter = await getData(value);
+      if (dataCharacter) {
+        setData(data);
+      } else {
+        setData([]);
+      }
+    };
+    getCharacter();
+  }, [data, value]);
 
-  render() {
-    if (this.state.hasError) {
-      throw new Error('ErrorBoundary error');
-    }
-    const data: Character[] = this.state.data;
-    return (
-      <main className={styles.wrapper}>
-        <ErrorBoundary>
-          <div className={styles.searchBlock}>
-            <InputSearch
-              onHandleChange={this.handleInputChange}
-              textSearch={this.state.value}
-            />
-            <ButtonSearch
-              onButtonClick={this.handleButtonClick}
-              value={this.state.value}
-            />
-            <button
-              onClick={this.handleButtonErrorClick}
-              className={styles.buttonError}
-            >
-              Ошибка
-            </button>
-          </div>
-          <CardList cards={data} />
-        </ErrorBoundary>
-      </main>
-    );
+  const handleButtonClick = async (val: {
+    data: Character[];
+  }): Promise<void> => {
+    setData(val.data);
+  };
+
+  const handleButtonErrorClick = (): void => {
+    setHasError(true);
+  };
+
+  if (hasError) {
+    throw new Error('ErrorBoundary error');
   }
-}
+  return (
+    <main className={styles.wrapper}>
+      <ErrorBoundary>
+        <div className={styles.searchBlock}>
+          <InputSearch onHandleChange={handleInputChange} textSearch={value} />
+          <ButtonSearch onButtonClick={handleButtonClick} value={value} />
+          <button
+            onClick={handleButtonErrorClick}
+            className={styles.buttonError}
+          >
+            Ошибка
+          </button>
+        </div>
+        <CardList cards={data} />
+      </ErrorBoundary>
+    </main>
+  );
+};
 
 export default HomePage;
